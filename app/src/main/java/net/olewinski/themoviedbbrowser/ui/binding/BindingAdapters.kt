74a7f8045363462fe3fa43adc0dd0b179f317bb9
mm-kk <mm-kk@o2.pl>
2020-05-28 01:04:01 +0200

    Adding placeholder for error while loading poster by Glide

--- app/src/main/java/net/olewinski/themoviedbbrowser/ui/binding/BindingAdapters.kt
+++ app/src/main/java/net/olewinski/themoviedbbrowser/ui/binding/BindingAdapters.kt
@@ -4,11 +4,13 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import net.olewinski.themoviedbbrowser.R
 
 @BindingAdapter("imageUrl")
 fun loadFromUrlToImageView(view: ImageView, imageUrl: String) {
     Glide.with(view.context)
         .load(imageUrl)
+        .error(R.drawable.ic_image_black_64dp)
         .transition(DrawableTransitionOptions.withCrossFade())
         .into(view)
 }
