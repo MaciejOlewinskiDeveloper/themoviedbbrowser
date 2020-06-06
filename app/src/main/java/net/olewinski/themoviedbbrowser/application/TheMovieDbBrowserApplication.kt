bb6c0dbf73af81d7bea5a28cb25ba28515ba78b0
mm-kk <mm-kk@o2.pl>
2020-06-07 01:18:34 +0200

    Fixing VectorDrawables + DataBinding conflict

--- app/src/main/java/net/olewinski/themoviedbbrowser/application/TheMovieDbBrowserApplication.kt
+++ app/src/main/java/net/olewinski/themoviedbbrowser/application/TheMovieDbBrowserApplication.kt
@@ -1,9 +1,20 @@
 package net.olewinski.themoviedbbrowser.application
 
 import android.app.Application
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.databinding.BindingMethod
+import androidx.databinding.BindingMethods
 import net.olewinski.themoviedbbrowser.di.components.DaggerApplicationComponent
 import net.olewinski.themoviedbbrowser.di.modules.ContextModule
 
+@BindingMethods(
+    BindingMethod(
+        type = ImageView::class,
+        attribute = "app:srcCompat",
+        method = "setImageDrawable"
+    )
+)
 class TheMovieDbBrowserApplication: Application() {
     val applicationComponent =
         DaggerApplicationComponent.builder().contextModule(ContextModule(this)).build()!!
